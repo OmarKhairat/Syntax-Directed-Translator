ba503a6850520dc894afd746f14808017dc82f4d
abdelrahman Gad <30812822+abdelrahman-gad-alex@users.noreply.github.com>
2023-12-24 18:44:53 +0200

    run of follow
    Co-Authored-By: Abdelaziz Mohamed <90736727+Abdelaziz25@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -36,108 +36,6 @@ int main()
         }
     }
 
-
-/*
-    std::unordered_map<std::string, std::set<std::vector<std::string>>> rules_map_set2;
-
-     // Rule 1
-    rules_map_set2["E"].insert({"T", "E'"});
-
-    // Rule 2
-    std::vector<std::string> rule2_vector1 = {"+", "T", "E'"};
-    std::vector<std::string> rule2_vector2 = {"Epsilon"};
-    rules_map_set2["E'"].insert(rule2_vector1);
-    rules_map_set2["E'"].insert(rule2_vector2);
-
-    // Rule 3
-    rules_map_set2["T"].insert({"F", "T'"});
-
-    // Rule 4
-    std::vector<std::string> rule4_vector1 = {"*", "F", "T'"};
-    std::vector<std::string> rule4_vector2 = {"Epsilon"};
-    rules_map_set2["T'"].insert(rule4_vector1);
-    rules_map_set2["T'"].insert(rule4_vector2);
-
-    // Rule 5
-    rules_map_set2["F"].insert({"(", "E", ")"});
-    rules_map_set2["F"].insert({"'id'"});
-
-     for (const auto& entry : rules_map_set2) {
-        std::cout << entry.first << " -> ";
-        for (const auto& vec : entry.second) {
-            std::cout << "[ ";
-            for (const auto& str : vec) {
-                std::cout << str << " ";
-            }
-            std::cout << "] ";
-        }
-        std::cout << std::endl;
-    }
-*/
-/*
-  std::unordered_map<std::string, std::set<std::vector<std::string>>> rules_map_set2;
-
-    // METHOD_BODY = STATEMENT_LIST
-    rules_map_set2["METHOD_BODY"].insert({{"STATEMENT_LIST"}});
-
-    // STATEMENT_LIST = STATEMENT | STATEMENT_LIST STATEMENT
-    rules_map_set2["STATEMENT_LIST"].insert({{"STATEMENT"}});
-    rules_map_set2["STATEMENT_LIST"].insert({{"STATEMENT_LIST", "STATEMENT"}});
-
-    // STATEMENT = DECLARATION | IF | WHILE | ASSIGNMENT
-    rules_map_set2["STATEMENT"].insert({{"DECLARATION"}});
-    rules_map_set2["STATEMENT"].insert({{"IF"}});
-    rules_map_set2["STATEMENT"].insert({{"WHILE"}});
-    rules_map_set2["STATEMENT"].insert({{"ASSIGNMENT"}});
-
-    // DECLARATION = PRIMITIVE_TYPE 'id' ';'
-    rules_map_set2["DECLARATION"].insert({{"PRIMITIVE_TYPE", "id", ";"}});
-
-    // IF = 'if' '(' EXPRESSION ')' '{' STATEMENT '}'
-    rules_map_set2["IF"].insert({{"if", "(", "EXPRESSION", ")", "{", "STATEMENT", "}"}});
-
-    // WHILE = 'while' '(' EXPRESSION ')' '{' STATEMENT '}'
-    rules_map_set2["WHILE"].insert({{"while", "(", "EXPRESSION", ")", "{", "STATEMENT", "}"}});
-
-    // ASSIGNMENT = 'id' '=' EXPRESSION ';'
-    rules_map_set2["ASSIGNMENT"].insert({{"id", "=", "EXPRESSION", ";"}});
-
-    // EXPRESSION = SIMPLE_EXPRESSION | SIMPLE_EXPRESSION 'relop' SIMPLE_EXPRESSION
-    rules_map_set2["EXPRESSION"].insert({{"SIMPLE_EXPRESSION"}});
-    rules_map_set2["EXPRESSION"].insert({{"SIMPLE_EXPRESSION", "relop", "SIMPLE_EXPRESSION"}});
-
-    // SIMPLE_EXPRESSION = TERM | SIGN TERM | SIMPLE_EXPRESSION 'addop' TERM
-    rules_map_set2["SIMPLE_EXPRESSION"].insert({{"TERM"}});
-    rules_map_set2["SIMPLE_EXPRESSION"].insert({{"SIGN", "TERM"}});
-    rules_map_set2["SIMPLE_EXPRESSION"].insert({{"SIMPLE_EXPRESSION", "addop", "TERM"}});
-
-    // TERM = FACTOR | TERM 'mulop' FACTOR
-    rules_map_set2["TERM"].insert({{"FACTOR"}});
-    rules_map_set2["TERM"].insert({{"TERM", "mulop", "FACTOR"}});
-
-    // FACTOR = 'id' | 'num' | '(' EXPRESSION ')'
-    rules_map_set2["FACTOR"].insert({{"id"}});
-    rules_map_set2["FACTOR"].insert({{"num"}});
-    rules_map_set2["FACTOR"].insert({{"(", "EXPRESSION", ")"}});
-
-    // SIGN = '+' | '-'
-    rules_map_set2["SIGN"].insert({{"+"}});
-    rules_map_set2["SIGN"].insert({{"-"}});
-
-    // Display the contents of the map
-    for (const auto& entry : rules_map_set2) {
-        std::cout << entry.first << " -> ";
-        for (const auto& vec : entry.second) {
-            std::cout << "[ ";
-            for (const auto& str : vec) {
-                std::cout << str << " ";
-            }
-            std::cout << "] ";
-        }
-        std::cout << std::endl;
-    }
-*/
-
     First_Follow firstFollowObject;
     std::unordered_map<std::string, std::vector<std::string>> First = firstFollowObject.constructFirst(rules_map_set, non_terminals);
     for (const auto &entry : First) {
@@ -151,41 +49,7 @@ int main()
         std::cout << "\n\n";
     }
 
-/*
-   std::unordered_map<std::string, std::set<std::vector<std::string>>> rules_map_set3;
-
-    rules_map_set3["F"].insert({"(", "E", ")"});
-    rules_map_set3["F"].insert({"'id'"});
-     // Rule 1
-      std::vector<std::string> rule11_vector1 = {"*", "F", "T'"};
-    std::vector<std::string> rule111_vector2 = {"Epsilon"};
-    rules_map_set3["T'"].insert(rule11_vector1);
-    rules_map_set3["T'"].insert(rule111_vector2);
-       rules_map_set3["T"].insert({"F", "T'"});
-       std::vector<std::string> rule22_vector1 = {"+", "T", "E'"};
-    std::vector<std::string> rule222_vector2 = {"Epsilon"};
-    rules_map_set3["E'"].insert(rule22_vector1);
-    rules_map_set3["E'"].insert(rule222_vector2);
-    rules_map_set3["E"].insert({"T", "E'"});
-
-
-*/
-
-    // Rule 5
-
-/*
-     for (const auto& entry : rules_map_set3) {
-        std::cout << entry.first << " -> ";
-        for (const auto& vec : entry.second) {
-            std::cout << "[ ";
-            for (const auto& str : vec) {
-                std::cout << str << " ";
-            }
-            std::cout << "] ";
-        }
-        std::cout << std::endl;
-    }
-    std::unordered_map<std::string, std::vector<std::string>> Follow = firstFollowObject.constructFollow(rules_map_set3,First);
+    std::unordered_map<std::string, std::vector<std::string>> Follow = firstFollowObject.constructFollow(rules_map_set,First, non_terminals);
 
      for (const auto &entry : Follow) {
         const std::string &key = entry.first;
@@ -196,7 +60,9 @@ int main()
             std::cout << value << " ";
         }
         std::cout << std::endl;
-    }*/
+    }
+
+
     /*
     LexicalAnalyzerFactory factory(projectPath);
     LexicalAnalyzer lexicalAnalyzer = factory.getLexicalAnalyzer();
